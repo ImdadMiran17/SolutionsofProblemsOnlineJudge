c846250fa7005688df3dbc84e0b1ae593f619664
ImdadMiran17 <54595670+ImdadMiran17@users.noreply.github.com>
2019-12-08 16:33:56 +0600

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+
+    int n,p,min=0;
+    cin >> n >> p;
+    min = (n/2)-(p/2);
+    if(min>(p/2))
+        min = p/2;
+
+    cout << min << endl;
+    return 0;
+}
