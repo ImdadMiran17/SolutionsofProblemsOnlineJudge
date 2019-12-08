427ea0f0a62e3e108dd9d0ca9ac7641832e63c0b
ImdadMiran17 <54595670+ImdadMiran17@users.noreply.github.com>
2019-12-08 18:08:20 +0600

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+
+    int q,x,y,z,catA,catB,c1=0,c2=0;
+    cin >> q;
+    for(int i=0;i<q;i++)
+    {
+        scanf("%d %d %d",&x,&y,&z);
+        catA=abs(x-z);
+        catB=abs(y-z);
+        if(catA>catB)
+            cout << "Cat B" << endl;
+        else if(catA<catB)
+            cout << "Cat A" << endl;
+        else
+            cout << "Mouse C" << endl;
+
+
+    }
+   /* if(c2>0)
+        cout << "Cat B" << endl;
+    else if(c1>0)
+        cout << "Cat A" << endl;
+    else
+        cout << "Mouse C" << endl;
+        */
+
+    return 0;
+}
