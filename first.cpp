9b4bd76664319e766fd49e821cc1e4f50373e131
ImdadMiran17 <54595670+ImdadMiran17@users.noreply.github.com>
2019-12-09 20:27:46 +0600

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+
+    int days=215;//as 256th day falls in 9th month,so count till
+                      // 8 month without februay
+    int year,febr;
+    cin >> year;
+    if(year<=1917)
+    {
+        if(year%4==0)
+            febr=29;
+        else
+            febr=28;
+    }
+    else if(year==1918)
+        febr=15;
+    else
+    {
+        if((year%400==0)|| (year%4==0) && (year%100!=0))
+            febr=29;
+        else
+            febr=28;
+    }
+
+    printf("%.2d.09.%d\n",256-(days+febr),year);
+    return 0;
+}
