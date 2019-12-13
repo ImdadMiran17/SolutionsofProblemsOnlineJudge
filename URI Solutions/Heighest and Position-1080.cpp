7f83651754160d7a54f3a3fe661f2407f75a74f2
ImdadMiran17 <54595670+ImdadMiran17@users.noreply.github.com>
2019-12-13 21:53:35 +0600

    Update and rename second.cpp to Heighest and Position-1080.cpp

---
+++ URI Solutions/Heighest and Position-1080.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    int n, max = -1, p, i;
+ for (i = 1; i < 101; ++i)
+ {
+  scanf("%d", &n);
+  if(max < n)
+   max = n, p = i;
+ }
+
+ printf("%d\n%d\n", max, p);
+
+ return 0;
+}
