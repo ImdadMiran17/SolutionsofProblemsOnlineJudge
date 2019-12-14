9e8f23ce86fbff6c2605cd0b3d2cf0c34028f2de
ImdadMiran17 <54595670+ImdadMiran17@users.noreply.github.com>
2019-12-14 09:57:35 +0600

    Update and rename second.cpp to PUM-1142.cpp

---
+++ URI Solutions/PUM-1142.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    int N, a,b,c=1,d=4;
+    scanf("%d", &N);
+    for(a=1;a<=N;a++)
+    {
+        for(b=c;b<=d;b++)
+        {
+            if(b%4==0)
+                printf("PUM\n");
+            else
+                printf ("%d ",b);
+        }
+        c+=4; d+=4;
+    }
+    return 0;
+}
