66198cc51aec46e4a358f759e296b1ac3b79b7b7
ImdadMiran17 <54595670+ImdadMiran17@users.noreply.github.com>
2019-12-14 17:22:35 +0600

    Add files via upload

---
+++ URI Solutions/second.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+
+    int a,b,c,max=0;
+    while(scanf("%d",&a)!=EOF)
+    {
+        for(b=1,max=0; b<=a; b++)
+        {
+            scanf("%d", &c);
+            if(c>max)
+                max=c;
+        }
+        if(max<10)
+            printf("1\n");
+        else if(max<20)
+            printf("2\n");
+        else
+            printf("3\n");
+    }
+    return 0;
+}
