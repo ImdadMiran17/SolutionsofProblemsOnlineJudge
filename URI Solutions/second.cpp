3aab03003d11c2d121f72f375f2ad9b8b7d65da6
ImdadMiran17 <54595670+ImdadMiran17@users.noreply.github.com>
2019-12-14 10:56:06 +0600

    Add files via upload

---
+++ URI Solutions/second.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    int x,y, a=0,b=1,c=0;
+    cin >> x;
+    for(y=1; y<x; y++)
+    {
+        if(y%2==1)
+        {
+            printf("%d ",c);
+            c=a+b;
+            a=c;
+        }
+        else if(y==2)
+            printf("%d ",c);
+        else if(y%2==0)
+        {
+            printf("%d ",c);
+            c=a+b;
+            b=c;
+        }
+    }
+    printf("%d\n",c);
+    return 0;
+}
