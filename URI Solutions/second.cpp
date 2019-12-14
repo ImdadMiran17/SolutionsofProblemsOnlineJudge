84ea152476945f6812214a3eb6abe8971d2f32cf
ImdadMiran17 <54595670+ImdadMiran17@users.noreply.github.com>
2019-12-14 18:37:00 +0600

    Add files via upload

---
+++ URI Solutions/second.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+
+   int a,b,c,d;
+    char ara[40]="LIFE IS NOT A PROBLEM TO BE SOLVED";
+    scanf("%d", &a);
+    for(b=0; b<a; b++)
+        printf("%c", ara[b]);
+    printf("\n");
+    return 0;
+}
