78b2fc4eb22f7f5d8cc3541386d77e28cc081912
ImdadMiran17 <54595670+ImdadMiran17@users.noreply.github.com>
2019-12-15 10:15:31 +0600

    Add files via upload

---
+++ URI Solutions/second.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+
+   int a,b;
+    long long int c,d,e,f;
+    char aa[201], bb[10], cc[201], dd[10];
+    scanf("%d", &a);
+    for(b=1; b<=a; b++)
+    {
+        scanf("%s %s %s %s", aa,bb,cc,dd);
+        scanf("%lld%lld", &c, &d);
+        e=c+d;
+        if(bb[0]=='P')
+        {
+            if(e%2==0) printf("%s\n", aa);
+            else printf("%s\n", cc);
+        }
+        else
+        {
+            if(e%2==0) printf("%s\n", cc);
+            else printf("%s\n", aa);
+        }
+    }
+    return 0;
+}
