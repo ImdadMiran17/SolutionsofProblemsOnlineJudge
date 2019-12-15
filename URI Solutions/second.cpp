e4108040de83c345fc97ceddc90486f0989bef70
ImdadMiran17 <54595670+ImdadMiran17@users.noreply.github.com>
2019-12-15 10:09:51 +0600

    Add files via upload

---
+++ URI Solutions/second.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<string.h>
+using namespace std;
+
+int main(void)
+{
+   int a,b,c,d,e;
+    char ara[100];
+    scanf("%d", &a);
+    for(b=1; b<=a; b++)
+    {
+        gets(ara);
+        getchar();
+    }
+    printf("Ciencia da Computacao\n");
+    return 0;
+}
