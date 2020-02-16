42e3ed789b89528bc105125d9d13ed159db41c65
Imdad Miran <54595670+ImdadMiran17@users.noreply.github.com>
2020-02-17 00:34:29 +0600

    Add files via upload

---
+++ Codeforces Solutions/second.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(void)
+{
+    int n,t,i,j;
+    cin >> n >> t;
+    char s[n];
+    cin >> s;
+    for(i=0;i<t;i++)
+    {
+        for(j=0;j<n-1;j++)
+        {
+            if(s[j]=='B' && s[j+1]=='G')
+            {
+                s[j]='G';
+                s[j+1]='B';
+                j++;
+            }
+        }
+    }
+
+    cout << s << endl;
+    return 0;
+}
