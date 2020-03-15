5f3a41a9cc4b7398ce706fe7f3848ed3f8dc5cc3
Imdad Miran <54595670+ImdadMiran17@users.noreply.github.com>
2020-03-15 21:34:19 +0600

    Add files via upload

---
+++ Codeforces Solutions/second.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+  int t;
+  cin >> t;
+  long long x;
+  while(t--)
+  {
+     scanf("%lld",&x);
+     printf("1 %lld\n",x-1);
+  }
+  return 0;
+}
