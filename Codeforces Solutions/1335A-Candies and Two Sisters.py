ba7b93d64021b82e7246f187ce54f9db53d3723f
Imdad Miran <54595670+ImdadMiran17@users.noreply.github.com>
2020-05-14 03:56:52 +0600

    Create 1335A-Candies and Two Sisters.py

---
+++ Codeforces Solutions/1335A-Candies and Two Sisters.py
@@ -0,0 +1,6 @@
+for _ in range(int(input())):
+  n = int(input())   # If you analyze the input you will understand what is the logic behind this implementation
+  if n % 2 == 0:
+    print(n//2-1)
+  else:
+    print(n//2)
