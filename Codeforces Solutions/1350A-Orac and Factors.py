bd8598d64fe74b67799effa9f329945b437d2185
Imdad Miran <54595670+ImdadMiran17@users.noreply.github.com>
2020-05-14 02:36:58 +0600

    Update 1350A-Orac and Factors.py

--- Codeforces Solutions/1350A-Orac and Factors.py
+++ Codeforces Solutions/1350A-Orac and Factors.py
@@ -1,8 +1,8 @@
 for _ in range(int(input())):
     n, k = map(int, input().split())
-    if n % 2 == 0:
-        n += 2 * k
-    else:
+    if n % 2 == 0:               # There are two parts of this solution.Odd and Even. In the Even part, if n==evenNumber then you just
+        n += 2 * k                  # have to multiply 2 to k then add n with 2*k.Because as you find about evenNum, you don't have to
+    else:                           # add 2 again and again to n.
         if k == 1:
             n = 2 * n
         else:
