013928830a7f9bb18b052bd5955af319253deabd
Imdad Miran <54595670+ImdadMiran17@users.noreply.github.com>
2020-07-08 03:57:46 +0600

    Create Maximum_Draws.cpp

---
+++ Hackerrank solutions/Maximum_Draws.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+  int T;
+  long long N;
+  cin >> T;
+  while(T--){
+    scanf("%lld",&N);
+    printf("%lld\n",N+1);
+  }
+  return 0;
+}
+
+
