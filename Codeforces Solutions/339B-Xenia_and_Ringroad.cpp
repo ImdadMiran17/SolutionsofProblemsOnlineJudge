5bf924a83bc5e49d0aa09a2a3a3049c2441242f1
Imdad Miran <54595670+ImdadMiran17@users.noreply.github.com>
2021-02-12 23:15:31 +0600

    Create 339B-Xenia_and_Ringroad.cpp

---
+++ Codeforces Solutions/339B-Xenia_and_Ringroad.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+  int n,m;
+  cin >> n >> m;
+  int initial = 1;
+  int time = 0;
+  while(m--){
+    int current;
+    scanf("%d",&current);
+    if(current>=initial){
+      time+=current-initial;
+    }else{
+      time+=n-(initial-current);
+    }
+    initial = current;
+  }
+  cout << time << endl;
+  return 0;
+}
+
+	
+	
+
