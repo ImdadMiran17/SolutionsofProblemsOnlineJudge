ed79ad242edaddaed749ce58eae99a0b28768c72
Imdad Miran <54595670+ImdadMiran17@users.noreply.github.com>
2021-02-15 02:46:55 +0600

    Create 189A-Cut_Ribbon.cpp

---
+++ Codeforces Solutions/189A-Cut_Ribbon.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(void){
+  int n,a,b,c;
+  cin >> n >> a >> b >> c;
+  int x,y,z,i,j,p=0;
+  i = min(a,min(b,c));
+  i = n/i;
+  for(x = i;x>=0;x--){
+    for(y=0;y<=i;y++){
+      z = abs((n-(a*x+b*y))/c);
+      if((a*x+b*y+c*z) == n){
+        p = max(p,x+y+z);
+      }
+    }
+  }
+  cout << p << endl;
+  return 0;
+}
+
+	
+	
+
