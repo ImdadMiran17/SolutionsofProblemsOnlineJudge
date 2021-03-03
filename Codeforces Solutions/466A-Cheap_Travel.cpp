b13c3904ff80eba56908865c104d0a06d8e4a0aa
Imdad Miran <54595670+ImdadMiran17@users.noreply.github.com>
2021-03-03 19:32:22 +0600

    Create 466A-Cheap_Travel.cpp

---
+++ Codeforces Solutions/466A-Cheap_Travel.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(void){
+  int n,m,a,b;
+  cin >> n >> m >> a >> b;
+  if(m*a <= b){
+    cout << n*a << endl;
+  }else{
+    cout << (n/m)*b + min((n%m)*a,b) << endl;
+  }
+  return 0;
+}
+
+	
+	
+
